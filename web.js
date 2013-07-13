0e1c786883184a5a7b6cb3fc632299fbf2afe92e
Ubuntu <ubuntu@ip-172-31-14-156.us-west-2.compute.internal>
2013-07-13 04:13:40 +0000

    Another attempt at reading a file

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var data = fs.readFileSync('./index.html');
-    response.send(data);
+    var data = new Buffer(fs.readFileSync('./index.html'));
+    response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
